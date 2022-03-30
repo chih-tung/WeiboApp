ed3012e7108a90eadced0e6b28366ae8b8cc1610
chihtung <chihtung605@gmail.com>
2022-03-31 01:02:17 +0800

    新增登出功能

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -28,4 +28,11 @@ class SessionsController extends Controller
        }
     }
 
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '登出成功！');
+        return redirect('login');
+    }
+
 }
