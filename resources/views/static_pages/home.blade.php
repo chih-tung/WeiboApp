3b945b12b63264719f8415fe9e74c8c151ecdb3b
chihtung <chihtung605@gmail.com>
2022-03-30 23:49:37 +0800

    create register page

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,13 +4,13 @@
   <div class="jumbotron">
     <h1>Hello Laravel</h1>
     <p class="lead">
-      你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
+      你現在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入門教程</a> 的示例項目主頁。
     </p>
     <p>
-      一切，将从这里开始。
+      一切，將從這裡開始。
     </p>
     <p>
-      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">註冊</a>
     </p>
   </div>
 @stop
